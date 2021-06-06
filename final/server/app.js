e6d464c928b85556b57004e9403b7cbbcbd5e4f5
TSinChen <sinin60076@gmail.com>
2021-06-06 22:24:28 +0800

    route

--- final/server/app.js
+++ final/server/app.js
@@ -1,5 +1,6 @@
-const express = require('express');
-const mysql = require('mysql');
+import express, { json } from 'express';
+import mysql from 'mysql';
+import cors from 'cors';
 
 const db = mysql.createConnection({
 	host: 'localhost',
@@ -17,6 +18,9 @@ db.connect((err) => {
 });
 
 const app = express();
+app.use(cors());
+
+const PORT = process.env.PORT || 5000;
 
 // /products
 app.get('/products', (req, res) => {
@@ -68,6 +72,121 @@ app.delete('/products/:pid', (req, res) => {
 	});
 });
 
-app.listen('5000', () => {
-	console.log('Server started on port 5000');
+// /customers
+app.get('/customers', (req, res) => {
+	const sql = 'SELECT * FROM customer';
+	const query = db.query(sql, (err, result) => {
+		if (err) throw err;
+		res.status(200).json(result);
+	});
+});
+
+// /customers/:cid
+app.patch('/customers/:cid', (req, res) => {
+	const { cid } = req.params;
+	const {
+		CustName,
+		City,
+		Address,
+		ZipCode,
+		Contact,
+		JobTitle,
+		Phone,
+		Industry,
+		TaxNo,
+	} = req.query;
+
+	const sql = `UPDATE customer SET CustName = '${CustName}', City = '${City}', Address = '${Address}', ZipCode = '${ZipCode}', Contact = '${Contact}', JobTitle = '${JobTitle}', Phone = '${Phone}', Industry = '${Industry}', TaxNo = '${TaxNo}' WHERE CustId = '${cid}'`;
+	const query = db.query(sql, (err, result) => {
+		if (err) throw err;
+		res.json(result);
+	});
+});
+
+app.delete('/customers/:cid', (req, res) => {
+	const { cid } = req.params;
+
+	const sql = `DELETE FROM customer WHERE CustId = '${cid}'`;
+	const query = db.query(sql, (err, result) => {
+		if (err) throw err;
+		res.json(result);
+	});
+});
+
+// /salesOrders
+app.get('/salesOrders', (req, res) => {
+	const sql = `SELECT * FROM salesorder`;
+	const query = db.query(sql, (err, result) => {
+		if (err) throw err;
+		res.status(200).json(result);
+	});
+});
+
+// /salesOrders/:oid
+app.patch('/salesOrders/:oid', (req, res) => {
+	const { oid } = req.params;
+	const { EmpId, CustId } = req.query;
+
+	const sql = `UPDATE salesorder SET EmpId = '${EmpId}', CustId = '${CustId}' WHERE OrderId = '${oid}'`;
+	const query = db.query(sql, (err, result) => {
+		if (err) throw err;
+		res.json(result);
+	});
+});
+
+app.delete('/salesOrders/:oid', (req, res) => {
+	const { oid } = req.params;
+
+	const sql = `DELETE FROM salesorder WHERE OrderId = '${oid}'`;
+	const query = db.query(sql, (err, result) => {
+		if (err) throw err;
+		res.json(result);
+	});
+});
+
+// /orderDetail/:oid
+app.get('/orderDetails/:oid', (req, res) => {
+	const { oid } = req.params;
+
+	const sql = `SELECT * FROM orderDetail WHERE OrderId = ${oid}`;
+	const query = db.query(sql, (err, result) => {
+		if (err) throw err;
+		res.status(200).json(result);
+	});
+});
+
+// // /salesOrders/:eid
+// app.get('/salesOrders/:eid', (req, res) => {
+// 	const { eid } = req.params;
+
+// 	const sql = `SELECT * FROM salesorder WHERE EmpId = ${eid}`;
+// 	const query = db.query(sql, (err, result) => {
+// 		if (err) throw err;
+// 		res.status(200).json(result);
+// 	});
+// });
+
+// // /orderDetail/:oid
+// app.get('/orderDetail/:oid', (req, res) => {
+// 	const { oid } = req.params;
+
+// 	const sql = `SELECT * FROM orderDetail WHERE OrderId = ${oid}`;
+// 	const query = db.query(sql, (err, result) => {
+// 		if (err) throw err;
+// 		res.status(200).json(result);
+// 	});
+// });
+
+// /employees/:eid
+app.get('/employees/:eid', (req, res) => {
+	const { eid } = req.params;
+	const sql = `SELECT * FROM employee WHERE EmpId = '${eid}'`;
+	const query = db.query(sql, (err, result) => {
+		if (err) throw err;
+		res.status(200).json(result);
+	});
+});
+
+app.listen(PORT, () => {
+	console.log(`Server started on port ${PORT}`);
 });
